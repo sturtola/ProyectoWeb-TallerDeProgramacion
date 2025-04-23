0366f6bbb016ac4e97e0f3f44f118f48068727b1
Sabrina <turtolasabrina@gmail.com>
2025-04-22 22:51:44 -0300

    Cambio de imagen de la paleta, cambio de footer

--- app/Views/auren.php
+++ app/Views/auren.php
@@ -27,8 +27,8 @@
         <ul class="nav-list">
           <li><a href="#">Inicio</a></li>
           <li><a href="#">Productos</a></li>
-          <li><a href="#">Sobre Nosotros</a></li>
-          <li><a href="#">Contacto</a></li>
+          <li><a href="#">Nosotros</a></li>
+          <li><a href="#">Información</a></li>
         </ul>
 
         <a class="navbar-brand" href="#">
@@ -50,53 +50,61 @@
       </nav>
     </div>
   </section>
-  <section class="informacion about-mini">
+  <section class="presentacion about-mini">
     <div class="about-content">
       <h2 style="font-weight: bold; margin-bottom: 50px">Auren</h2>
-      <p">Auren es una empresa con raíces profundas en Formosa y Corrientes, y más de 45 años de trayectoria en el mercado.</p>
-      <p>Nacimos impulsados por una verdadera pasión por el deporte, y fue justamente esa pasión la que nos llevó a realizar nuestra primera venta: una paleta como la que hoy sigue siendo símbolo de nuestros inicios. En Auren, seguimos apostando al desarrollo, al talento local y a la innovación.</p> 
-      <p style="font-style: italic; font-weight: bold; margin-top: 30px;">Más que una marca, somos una historia que se construye día a día junto a nuestros clientes.</p>
+      <p>Auren es una empresa con raíces profundas en Formosa y Corrientes, y más de 45 años de trayectoria en el
+        mercado.</p>
+        <p>Nacimos impulsados por una verdadera pasión por el deporte, y fue justamente esa pasión la que nos llevó a
+          realizar nuestra primera venta: una paleta como la que hoy sigue siendo símbolo de nuestros inicios. En Auren,
+          seguimos apostando al desarrollo, al talento local y a la innovación.</p>
+        <p style="font-style: italic; font-weight: bold; margin-top: 30px;">Más que una marca, somos una historia que se
+          construye día a día junto a nuestros clientes.</p>
     </div>
     <div class="about-carousel">
       <div class="carousel-track">
-        <img src="../assets/img/paleta auren.png" class="carousel-img">
-        <img src="../assets/img/paleta auren costado.png" alt="Paleta Auren costado" class="carousel-img">
+        <img src="../assets/img/paletafrente.png" class="carousel-img">
+        <img src="../assets/img/paletacostado1.png" class="carousel-img">
+        <img src="../assets/img/paletaatras.png" class="carousel-img">
+        <img src="../assets/img/paletacostado2.png" class="carousel-img">
+        <img src="../assets/img/paletafrente.png" class="carousel-img">
+        <img src="../assets/img/paletacostado1.png" class="carousel-img">
+        <img src="../assets/img/paletaatras.png" class="carousel-img">
+        <img src="../assets/img/paletacostado2.png" class="carousel-img">
       </div>
     </div>
+  </section>
+  <section class="categorias">
+  
   </section>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
-<footer class="footer">
-  <div class="footer-container">
-    <div class="footer-item">
-      <img src="../assets/img/ubicacion1.png" alt="Ubicación" class="footer-icon">
-      <p>Corrientes, Argentina</p>
+<footer>
+  <section class="footer">
+    <div class="row social-container">
+      <a href="#"><img class="social-iconos" src="../assets/img/wpp1.png"></img></a>
+      <a href="#"><img class="social-iconos" src="../assets/img/instagram1.png"></img></a>
+      <a href="#"><img class="social-iconos" src="../assets/img/facebook1.webp"></img></a>
+      <a href="#"><img class="social-iconos" src="../assets/img/tiktok1.webp"></img></a>
     </div>
-    <div class="footer-item">
-      <img src="../assets/img/mail1.png" alt="Correo" class="footer-icon">
-      <p>auren@gmail.com</p>
-    </div>
-    <div class="footer-item">
-      <img src="../assets/img/telefono1.png" alt="Teléfono" class="footer-icon">
-      <p>3704 770647</p>
-    </div>
-    <div class="footer-social">
-      <a href="https://instagram.com" target="_blank">
-        <img src="../assets/img/instagram1.png" alt="Instagram" class="social-icon">
-      </a>
-      <a href="https://www.facebook.com/">
-        <img src="../assets/img/facebook1.png" alt="Facebook" class="social-icon">
-      </a>
-      <a href="https://x.com/">
-        <img src="../assets/img/x1.png" alt="X" class="social-icon">
-      </a>
-    </div>
-  </div>
-  <p class="footer-copy">Auren © 2025</p>
-</footer>
 
+    <div class="row">
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Sobre Nosotros</a></li>
+        <li><a href="#">Terminos y Condiciones</a></li>
+        <li><a href="#">Contacto</a></li>
+      </ul>
+    </div>
+
+    <div class="row" align-items="center">
+      AUREN © 2025 - Todos los derechos reservados.
+    </div>
+  </section>
+</footer>
 
 </html>
