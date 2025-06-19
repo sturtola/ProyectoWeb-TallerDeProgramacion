102ce45d6f088fb0846569626bbf4a5c28a4f678
unknown <turtolasabrina@gmail.com>
2025-06-18 22:20:08 -0300

    Cambios

--- app/Views/components/navbar.php
+++ app/Views/components/navbar.php
@@ -1,78 +1,59 @@
-<section class="principal imagen-header">
-  <div class="container-fluid barraNav">
-    <nav class="navbar">
-      <!-- Checkbox y botón menú hamburguesa -->
-      <input type="checkbox" id="menu-toggle" class="menu-toggle">
-      <label for="menu-toggle" class="menu-icon">
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
-      </label>
+<section class="principal-header">
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-transparent px-4">
+  <div class="container-fluid">
+    <a class="navbar-brand d-flex align-items-center" href="<?= base_url('/') ?>">
+      <img src="<?= base_url('assets/img/nombremarca1.png') ?>" alt="Logo Auren" class="logo-navbar">
+    </a>
 
-      <!-- Menú horizontal (pantallas grandes) -->
-      <ul class="nav-list">
-        <li class="dropdown">
-          <a href="<?= base_url('/#inicio-inicio') ?>">Inicio</a>
-          <ul class="menu-vertical-barra">
-            <li><a href="<?= base_url('/IniciarSesion#inicio-sesion') ?>">Iniciar Sesión</a></li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="<?= base_url('/catalogo#inicio-productos') ?>">Productos</a>
-          <ul class="menu-vertical-barra">
-            <li style="transition: none; transform: none;"><a href="<?= base_url('/Mantenimiento#inicio-mantenimiento') ?>">Hombre</a></li>
-            <li><a href="<?= base_url('/Mantenimiento#inicio-mantenimiento') ?>">Mujer</a></li>
-            <li><a href="<?= base_url('/Mantenimiento#inicio-mantenimiento') ?>">Accesorios</a></li>
-          </ul>
-        </li>
-        <li><a href="<?= base_url('/QuienesSomos#inicio-nosotros') ?>">Nosotros</a></li>
-        <li class="dropdown">
+    <!-- Botón hamburguesa -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContenido">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-          <a href="#">Información</a>
-          <ul class="menu-vertical-barra menu-informacion">
-            <li><a href="<?= base_url('/Contacto#inicio-contacto') ?>">Contacto</a></li>
-            <li><a href="<?= base_url('/Comercializacion#inicio-comercializacion') ?>">Comercialización</a></li>
-            <li><a href="<?= base_url('/TerminosYUsos#inicio-condiciones') ?>">Términos y Condiciones</a></li>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarContenido">
+      <ul class="navbar-nav align-items-center gap-3">
+        <li class="nav-item">
+          <a class="nav-link" href="<?= base_url('/#inicio-inicio') ?>">Inicio</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Productos</a>
+          <ul class="dropdown-menu dropdown-menu-dark">
+            <li><a class="dropdown-item" href="<?= base_url('/catalogo#inicio-productos') ?>">Todos</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('/catalogo#inicio-productos') ?>">Hombre</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('/catalogo#inicio-productos') ?>">Mujer</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('/catalogo#inicio-productos') ?>">Accesorios</a></li>
           </ul>
         </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="<?= base_url('/QuienesSomos#inicio-nosotros') ?>">Nosotros</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Información</a>
+          <ul class="dropdown-menu dropdown-menu-dark">
+            <li><a class="dropdown-item" href="<?= base_url('/Contacto#inicio-contacto') ?>">Contacto</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('/Comercializacion#inicio-comercializacion') ?>">Comercialización</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('/TerminosYUsos#inicio-condiciones') ?>">Términos y Usos</a></li>
+          </ul>
+        </li>
+
+        <?php if (session('id_usuario')): ?>
+          <li class="nav-item">
+            <a class="nav-link text-warning" href="<?= base_url('/logout') ?>">
+              <i class="bi bi-box-arrow-right"></i> Cerrar sesión
+            </a>
+          </li>
+        <?php else: ?>
+          <li class="nav-item">
+            <a class="nav-link text-info" href="<?= base_url('/IniciarSesion') ?>">
+              <i class="bi bi-person-circle"></i> Iniciar sesión
+            </a>
+          </li>
+        <?php endif; ?>
       </ul>
-
-
-      <a class="navbar-brand" href="<?= base_url('/') ?>">
-        <img class="nombreMarca" src="assets/img/nombremarca1.png">
-      </a>
-
-      <div class="sidebar">
-        <ul class="menu-vertical-lateral">
-          <li><a href="<?= base_url('/') ?>">
-              <h5>Inicio<h5>
-            </a></li>
-
-          <li class="submenu-toggle">
-            <a href="<?= base_url('/Mantenimiento#inicio-mantenimiento') ?>">
-              <h5>Productos</h5><a>
-                <ul class="submenu-vertical-lateral">
-                  <li><a href="<?= base_url('/Mantenimiento#inicio-mantenimiento') ?>">Hombre</a></li>
-                  <li><a href="<?= base_url('/Mantenimiento#inicio-mantenimiento') ?>">Mujer</a></li>
-                  <li><a href="<?= base_url('/Mantenimiento#inicio-mantenimiento') ?>">Accesorios</a></li>
-                </ul>
-          </li>
-
-          <li><a href="<?= base_url('/QuienesSomos#inicio-nosotros') ?>">
-              <h5>Nosotros<h5>
-            </a></li>
-
-          <li class="submenu-toggle">
-            <a href="#"><h5>Información</h5><a>
-            <ul class="submenu-vertical-lateral">
-              <li><a href="<?= base_url('/Contacto#inicio-contacto') ?>">Contacto</a></li>
-              <li><a href="<?= base_url('/Comercializacion#inicio-comercializacion') ?>">Comercialización</a></li>
-              <li><a href="<?= base_url('/TerminosYUsos#incio-condiciones') ?>">Términos y Condiciones</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    </div>
   </div>
+</nav>
 </section>
-
